5f7c8af9dc250d0222c5ab87f078e848ee98a1f8
Libo Liu <liboliu@usc.edu>
2018-12-29 23:42:20 -0800

    variable example for mint map

--- src/mint-map-test-app/mint-map-test-app.js
+++ src/mint-map-test-app/mint-map-test-app.js
@@ -16,15 +16,9 @@ class MintMapTestApp extends PolymerElement {
         type: Array,
         notify:true,
         readOnly:false,
-        value:[{ name: 'precipitation', 
-                    stdname: 'atmosphere_water__one-month_time_integral_of_precipitation_leq-volume_flux', 
-                    md5: '7ba0f437be4d71c812e6006ce5a65f2e', 
-                    uri: 'https://mint.isi.edu/dcat/variables/atmosphere_water__one-month_time_integral_of_precipitation_leq-volume_flux?file_location=33db03abf5efa9367b390c552a24a3b3&time_range=2018-03-01T00%3A00%3A00%2F2018-04-01T00%3A00%3A00&spatial_coverage=0a4f705eb66d66ccab88e6801ce21513&short_name=precipitation'
-                  },
-                  { name: 'landuse', 
-                    stdname: 'Land_Cover_Type', 
-                    md5: '486e33f628fd222172b7d8438b9cb651', 
-                    uri: 'https://mint.isi.edu/dcat/variables/Land_Cover_Type?file_location=1176557f3eeaf2b58c7cd22b877857c9&time_range=&spatial_coverage=d41d8cd98f00b204e9800998ecf8427e&short_name=landuse'
+        value:[{ layerName: 'chirps.rainfall.precipitation', 
+                    md5: '6f5ecde47d5a3303cfe9463ad57d4dc8', 
+                    dcid: 0
                   }]
       }
     };
